c61fdecc21d4179dc32beefeba352e98e5c02d38
jhyooooon <jhyooooooon@gmail.com>
2020-08-12 01:53:03 +0900

    Arrange folder structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import "./reset.css";
-import Typing from "./components/Typing/Typing";
-import Newswrap from "./components/News/Newswrap";
-import ContactWrap from "./components/Contact/ContactWrap";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Typing />
-      <Newswrap />
-      <ContactWrap />
+      <Section />
     </div>
   );
 }
