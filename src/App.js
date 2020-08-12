d8be9c1ff107fcfaad827a07341481257fae295d
jhyooooon <jhyooooooon@gmail.com>
2020-08-12 19:31:47 +0900

    Create footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header/Header";
 import "./reset.css";
 import Section from "./components/Section";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Section />
+      <Footer />
     </div>
   );
 }
